9bfee681e214bcab244197fef9aed9bd2e18619c
Lucija Krsticevic <lkrstice1@pmfst.hr>
2023-05-18 18:54:09 +0200

    Dodan btn za promjenu vaznosti poruke

--- frontend/src/components/Poruka.js
+++ frontend/src/components/Poruka.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const Poruka = ({poruka}) => {
+const Poruka = ({poruka, promjenaVaznosti}) => {
+  const oznaka = poruka.vazno 
+  ? 'Oznaci kao nevazno' : 'oznaci kao vazno'
+
   return (
-    <li>{poruka.sadrzaj}</li>
+    <li>
+      {poruka.sadrzaj}
+      <button onClick={promjenaVaznosti}>{oznaka}</button>
+    </li>
   )
 }
 
