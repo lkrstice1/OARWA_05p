845ccd854153c1262deaa2b0a4a2acff60493f9c
Lucija Krsticevic <lkrstice1@pmfst.hr>
2023-05-18 19:24:33 +0200

    Dodan btn za brisanje svakoj poruci

--- frontend/src/components/Poruka.js
+++ frontend/src/components/Poruka.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import './Poruka.css'
 
-const Poruka = ({poruka, promjenaVaznosti}) => {
+const Poruka = ({poruka, promjenaVaznosti, brisiPoruku}) => {
   const oznaka = poruka.vazno 
   ? 'Oznaci kao nevazno' : 'oznaci kao vazno'
 
   return (
     <li>
-      {poruka.sadrzaj}
+      <span className={poruka.vazno ? 'vazno' : 'nevazno'}>{poruka.sadrzaj}</span>
       <button onClick={promjenaVaznosti}>{oznaka}</button>
+      <button onClick={brisiPoruku}><span role='img' aria-label="delete">❌</span></button>
     </li>
   )
 }
